c64b91b51705a015f808901c572e2d828e3165ac
kavinda dhananjaya <kavindadhananjaya@gmail.com>
2015-03-29 14:37:12 +0530

    Real Steel-Fight begin

---
+++ src/real_state_programme/Real_State_Programme.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package real_state_programme;
+
+/**
+ *
+ * @author kavi
+ */
+public class Real_State_Programme {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
